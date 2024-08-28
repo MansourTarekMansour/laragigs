0953972098e50bf907f2e6dbd6240f6d90402b06
Mansour Tarek Mansour <mansourtarek100@gmail.com>
2024-08-28 18:59:18 +0300

    rander listing view

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,10 +1,6 @@
-@extends('layout')
-
-@section('content')
-
+<x-layout>
 @include('partial._hero')
 @include('partial._search')
-
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
     @unless(count($listings) == 0)
     @foreach ($listings as $listing)
@@ -15,4 +11,4 @@
     @endunless
 
 </div>
-@endsection
+</x-layout>
